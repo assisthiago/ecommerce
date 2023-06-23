8fd71577ab2e58a6c9433b70a1d6b9aa69d89a84
Thiago Assis <thiago.medassis@gmail.com>
2023-06-23 11:26:39 -0300

    Address refactor

--- app/addresses/models.py
+++ app/addresses/models.py
@@ -8,11 +8,13 @@ from app.profiles.models import Profile
 class Address(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField('Rua/Avenida/Estrada...', max_length=100)
+    zip_code = models.IntegerField('cep')
+    street = models.CharField('Rua/Avenida/Estrada...', max_length=100)
     number = models.IntegerField('número')
+    complement = models.CharField('complemento', max_length=100, blank=True, null=True)
+    reference = models.CharField('referência', max_length=100, blank=True, null=True)
     city = models.CharField('cidade', max_length=100)
     country = models.CharField('país', max_length=100)
-    zip_code = models.IntegerField('cep')
     phone = models.IntegerField('telefone')
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     updated_at = models.DateTimeField('atualizado em', auto_now=True)
@@ -21,5 +23,7 @@ class Address(models.Model):
         Profile, related_name='address', on_delete=models.CASCADE)
 
     class Meta:
+        db_table = 'address'
+        unique_together = ['zip_code', 'street', 'number', 'complement']
         verbose_name = 'endereço'
         verbose_name_plural = 'endereços'
