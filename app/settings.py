3bffc905f6c7f78614c8d8a228095de6a0036e57
Thiago Assis <thiago.medassis@gmail.com>
2023-06-23 11:28:04 -0300

    Route and settings

--- app/settings.py
+++ app/settings.py
@@ -27,9 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app.addresses',
     'app.categories',
+    'app.core',
     'app.discounts',
     'app.inventories',
-    'app.payments',
     'app.products',
     'app.profiles',
 ]
@@ -111,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
