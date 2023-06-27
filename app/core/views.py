a624436d751ba1e28b7c063567ea93de509061c3
Thiago Assis <thiago.medassis@gmail.com>
2023-06-27 17:11:51 -0300

    Adding auth templates

--- app/core/views.py
+++ app/core/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from app.categories.models import Category
 from app.products.models import Product
 
 def home(request):
-    categories = Category.objects.all()
-    products = Product.objects.all()
-    context = {'products': products, 'categories': categories}
+    context = {
+        'categories': Category.objects.all(),
+        'products': Product.objects.all(),
+        'user': str(request.user.profile),
+    }
     return render(request, 'index.html', context=context)
+
+
+def sign_in(request):
+    return render(request, 'sign-in.html')
+
+
+def sign_up(request):
+    return render(request, 'sign-up.html')
+
+
+def sign_out(request):
+    return redirect('home')
+
+
+def recover_password(request):
+    return render(request, 'recover-password.html')
--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,9 @@ import app.core.views
 
 urlpatterns = [
     path('', app.core.views.home, name='home'),
+    path('sign-in', app.core.views.sign_in, name='sign-in'),
+    path('sign-up', app.core.views.sign_up, name='sign-up'),
+    path('sign-out', app.core.views.sign_out, name='sign-out'),
+    path('recover-password', app.core.views.recover_password, name='recover-password'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
