78b9ba20ec83c73f2a5541a70d4aaa6d704563af
Thiago Assis <thiago.medassis@gmail.com>
2023-06-27 15:45:26 -0300

    Adding categories list in _nav

--- app/core/views.py
+++ app/core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+from app.categories.models import Category
 from app.products.models import Product
 
 def home(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'categories': categories}
     return render(request, 'index.html', context=context)
