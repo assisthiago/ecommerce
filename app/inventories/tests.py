5e114e1baca149fca9b151fa5b26e608b5d5d8ee
Thiago Assis <thiago.medassis@gmail.com>
2023-06-29 12:06:23 -0300

    Adding Inventory model tests

--- app/inventories/tests.py
+++ app/inventories/tests.py
@@ -1,3 +1,42 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from app.categories.models import Category
+from app.inventories.models import Inventory
+from app.products.models import Product
+
+
+class InventoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='categoria')
+        self.product = Product.objects.create(
+            name='produto',
+            price=12.34,
+            category=self.category)
+
+        self.inventory = Inventory.objects.create(
+            color='cor',
+            size='gg',
+            quantity=1,
+            product=self.product)
+
+    def test_create(self):
+        self.assertTrue(Inventory.objects.exists())
+
+    def test_has_product(self):
+        self.assertTrue(self.inventory.product)
+
+    def test_product(self):
+        self.assertIsInstance(self.inventory.product, Product)
+
+    def test_str(self):
+        self.assertEqual('1 em estoque', str(self.inventory))
+
+    def test_size_choices(self):
+        inventory = Inventory.objects.create(
+            color='cor',
+            size='xxg',
+            quantity=1,
+            product=self.product)
+
+        self.assertRaises(ValidationError, inventory.full_clean)
