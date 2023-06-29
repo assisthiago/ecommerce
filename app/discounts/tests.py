34fb4cbb1940fbbc6ecc703224db052fb000b9d7
Thiago Assis <thiago.medassis@gmail.com>
2023-06-29 11:53:19 -0300

    Adding Discount model test

--- app/discounts/tests.py
+++ app/discounts/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app.categories.models import Category
+from app.discounts.models import Discount
+from app.products.models import Product
+
+
+class DiscountModelTest(TestCase):
+    def setUp(self):
+        category = Category.objects.create(name='categoria')
+        product = Product.objects.create(
+            name='produto',
+            price=12.34,
+            category=category)
+
+        self.discount = Discount.objects.create(
+            name='desconto',
+            percent=0.5,
+            product=product)
+
+    def test_create(self):
+        self.assertTrue(Discount.objects.exists())
+
+    def test_has_product(self):
+        self.assertTrue(self.discount.product)
+
+    def test_product(self):
+        self.assertIsInstance(self.discount.product, Product)
+
+    def test_str(self):
+        self.assertEqual('desconto', str(self.discount))
+
+    def test_description_blank(self):
+        field = Discount._meta.get_field('description')
+        self.assertTrue(field.blank)
