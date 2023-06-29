37cef8a1eab9c85067c3b274dc0d089e41ff4cdc
Thiago Assis <thiago.medassis@gmail.com>
2023-06-29 11:37:37 -0300

    Adding Category model test

--- app/categories/tests.py
+++ app/categories/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app.categories.models import Category
+
+
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='categoria')
+
+    def test_create(self):
+        self.assertTrue(Category.objects.exists())
+
+    def test_str(self):
+        self.assertEqual('categoria', str(self.category))
+
+    def test_description_blank(self):
+        field = Category._meta.get_field('description')
+        self.assertTrue(field.blank)
