60d4dae9689a695d5cbd5e1035920ecab5d0e886
Thiago Assis <thiago.medassis@gmail.com>
2023-07-01 16:59:30 -0300

    Category admin test

--- app/categories/tests.py
+++ app/categories/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from app.categories.admin import admin, CategoryAdmin
 from app.categories.models import Category
 
 
@@ -16,3 +17,15 @@ class CategoryModelTest(TestCase):
     def test_description_blank(self):
         field = Category._meta.get_field('description')
         self.assertTrue(field.blank)
+
+
+class CategoryAdminTest(TestCase):
+    fixtures = ['app/core/fixtures/categories.json']
+
+    def setUp(self):
+        self.model_admin = CategoryAdmin(Category, admin.site)
+
+    def test_slug(self):
+        expected = self.model_admin.slug(self.model_admin.model.objects.first())
+        self.assertEqual(expected, '2AE8BAC7')
+
